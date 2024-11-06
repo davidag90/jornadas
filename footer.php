09ff8f93e49c84478ab96056ce312e780a236dae
David Gomez <info@dagdev.com.ar>
2024-11-06 17:44:04 -0300

    Feat: footer extras and hotel suggestions layout

--- footer.php
+++ footer.php
@@ -4,8 +4,11 @@ defined('ABSPATH') || exit;
 ?>
 
 <footer id="main-footer">
-  <nav class="navbar fixed-bottom bg-primary border-top border-dark border-5 py-0">
-    <div class="container-fluid justify-content-center">
+  <nav class="navbar fixed-bottom bg-primary py-0">
+    <div id="credits" class="w-100 d-flex justify-content-center align-items-center text-bg-secondary bg-gradient py-1">
+      <div>Organiza <a href="https://coc-cordoba.org.ar/" target="_blank" class="link-light"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/footer/coc.png" alt="Circulo Odontológico de Córdoba"><a></div>
+    </div>
+    <div class="container-fluid justify-content-center border-top border-primary-subtle border-2">
       <div class="navbar-nav w-100">
         <a href="<?php echo home_url(); ?>/disertantes" class="nav-link link-light text-center"><i class="fa-solid fs-1 mb-1 fa-user-tie d-block"></i><span class="link-detail d-block text-decoration-none">Disertantes</a>
         <!-- <a href="#" class="nav-link link-light text-center"><i class="fa-solid fs-1 mb-1 fa-file-lines d-block"></i><span class="link-detail d-block text-decoration-none">Programa</a> -->
