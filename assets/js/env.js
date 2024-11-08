bf72caef52bb3613cee0895740de63b24b5ba855
David Gomez <info@dagdev.com.ar>
2024-11-08 13:07:42 -0300

    New: create environment parameters file

---
+++ assets/js/env.js
@@ -0,0 +1,4 @@
+const HOME_URL = envParams.HOME_URL;
+const API_BASE_URL = HOME_URL + "wp-json/wp/v2/";
+const THEME_URL = HOME_URL + "wp-content/themes/json/";
+const CHECKOUT_URL = HOME_URL + "finalizar-compra/?add-to-cart=";
