ddd3f6167aafef5e9d9bc580eb59713ed6d465c7
David Gomez <info@dagdev.com.ar>
2024-12-03 18:09:28 -0300

    Fix: table layout for Inscripciones

--- assets/js/inscripcion.js
+++ assets/js/inscripcion.js
@@ -16,3 +16,11 @@ function createOrderURL(event) {
         btnPreOrder.classList.add('disabled');
     }
 }
+
+const tableArancelesWrapper = document.getElementById('aranceles-table');
+const tableAranceles = document.querySelector('#aranceles-table > table');
+
+tableArancelesWrapper.setAttribute('class', 'w-100');
+tableAranceles.classList.remove('has-fixed-layout');
+tableAranceles.classList.add('table');
+tableAranceles.classList.add('table-light');
